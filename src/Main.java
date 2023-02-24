77e2e471b160ef1b711159961b9aff34bfacc51b
MaxOmenes <ukhin.maxim@gmail.com>
2023-02-25 00:10:04 +0300

    Second Change

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,17 @@ import models.Labyrinth;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        int[][] a = {{5,10 ,14},
+                     {9,5,6},
+                     {7,8,12}};
+        Labyrinth l = new Labyrinth();
+        l.setLabyrinth(l.generateLabyrinthById(a));
+        int[][] ans = Labyrinth.findPath(l, 2, 2);
+        for (int i = 0; i < ans.length; i++){
+            for(int j = 0; j < ans[i].length; j++){
+                System.out.print(ans[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
